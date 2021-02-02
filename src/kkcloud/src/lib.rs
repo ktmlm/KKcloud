b6d532bfff49415eaa82ad90815800f2069c6178
FanHui <kt71@qq.com>
2021-02-02 16:09:22 +0800

    doc

--- src/kkcloud/src/lib.rs
+++ src/kkcloud/src/lib.rs
@@ -1,3 +1,16 @@
+//!
+//! # KKcloud
+//!
+//! **K**eep it correct, **K**eep it reliable, a lightweight private-cloud solution for SME scenarios.
+//!
+//! Core components:
+//!
+//! - [**[bin]** kkcloud-client](https://crates.io/crates/kkcloud-client)
+//! - [**[bin]** kkcloud-server](https://crates.io/crates/kkcloud-server)
+//! - [**[lib]** kkcloud-engine](https://crates.io/crates/kkcloud-engine)
+//! - [**[lib]** kkcloud-framework](https://crates.io/crates/kkcloud-framework)
+//!
+
 #[cfg(test)]
 mod tests {
     #[test]
